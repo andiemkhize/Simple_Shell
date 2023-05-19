aca9731aa5f7840902628a01307a4c10966c5de7
nelisa-dludla <nelisa.dludla@outlook.com>
2023-05-19 23:00:01 +0200

    Updated simpleShell.c & shell.h - Streamlined main function & added new function prototypes to shell.h

--- shell.h
+++ shell.h
@@ -8,8 +8,36 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-void execute_command(char **argv);
-char *find_path(char *command);
+/**
+ * struct builtins - Struct for holding the builtins
+ * @name: name of builtin command
+ * @function: Pointer to a function
+ */
+typedef struct builtins
+{
+        char *name;
+        void (*function)();
+} builtin;
+
+/* Simple Shell core functions */
+char *read_input(void);
+char **parse_input(char *str, char **argv, int argc);
+char *find_path(char *parsed_input);
+void execute_command(char **argv, char *command);
+
+/* Builtin functions */
+void exit_shell(void);
+void print_env(void);
+
+/* Helper functions */
+char *getpath(void);
+int _strncmp(const char *s1, const char *s2, int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, const char *src);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
 
 #endif
--- simpleShell.c
+++ simpleShell.c
@@ -1,100 +1,50 @@
 #include "shell.h"
 
 /**
- * read_input - Function reads input from stdin
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * return: Returns the input
+ * Return: 0 (Sucess)
  */
-char *read_input(void)
-{
-	char *input = NULL;
-	size_t len = 0;
-	ssize_t char_read;
-
-	/* Getting the input/command from user */
-	printf(":) ");
-	char_read = getline(&input, &len, stdin);
-
-	if (char_read == -1)
-	{
-		printf("Leaving Shell.. Bye!\n"); /* Test using CTRL + D */
-		exit(EXIT_SUCCESS);
-	}
-
-	return (input);
-}
-/**
- * parse_input - Function tokenizes the input
- *
- * @str: String to be tokenized
- *
- * Return: argv (An array of tokens)
- */
-char **parse_input(char *str, char **argv, int argc)
-{
-	char *str_cpy = NULL, *token = NULL;
-	const char *delim = " \n";
-	int i = 0;
-
-	/* Make a copy of input to use for second tokenization */
-	str_cpy = strdup(str);
-
-	if (str_cpy == NULL)
-	{
-		perror("ssh: Memory Allocation Error");
-		exit(EXIT_SUCCESS);
-	}
-
-	/* Convert input into tokens */
-	token = strtok(str, delim);
-
-	argc = 0;
-
-	while (token != NULL)
-	{
-		argc++;
-		token = strtok(NULL, delim);
-	}
-
-	// printf("Number of tokens: %d\n", argc);
-
-	argv = malloc(sizeof(char *) * argc);
-
-	token = strtok(str_cpy, delim);
-
-	while (token != NULL)
-	{
-		argv[i] = strdup(token);
-		token = strtok(NULL, delim);
-		i++;
-	}
-	argv[i] = NULL;
-
-	i = 0;
-
-	/* Find out if the tokens are counted and stored correctly */
-	// while (i < argc)
-	// {
-	// 	printf("Token[%d]: %s\n", i, argv[i]);
-	// 	i++;
-	// }
-
-	return (argv);
-}
-
 int main(int argc, char **argv)
 {
-	char *user_input, **parsed_input;
+        builtin command_action[] = {
+            {"exit\n", exit_shell},
+            {"env\n", print_env},
+        };
 
-	printf("===== Welcome to the Simple Shell =====\n\n");
-	while (true)
-	{
-		user_input = read_input();
-		parsed_input = parse_input(user_input, argv, argc);
+        char *user_input, **parsed_input, *command;
+        int num_builtins, i;
 
-		execute_command(parsed_input);
-		// printf("%s\n", user_input); /* Check if user input is being recieved and stored */
-	}
+        num_builtins = sizeof(command_action) / sizeof(builtin);
 
-	return (0);
+        while (true)
+        {
+                user_input = read_input();
+
+                if (user_input == NULL)
+                {
+                        continue;
+                }
+
+                for (i = 0; i < num_builtins; i++)
+                {
+                        if (_strcmp(user_input, command_action[i].name) == 0)
+                        {
+                                command_action[i].function();
+                                break;
+                        }
+                }
+
+                parsed_input = parse_input(user_input, argv, argc);
+                command = find_path(*parsed_input);
+                execute_command(parsed_input, command);
+
+                free(user_input);
+                free(parsed_input);
+                free(command);
+        }
+
+        return (0);
 }
